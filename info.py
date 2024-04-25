6a17e118c7c40427edeb9cd63854694ec49b6a9c
Saheli Jati <sahelijati130@gmail.com>
2024-04-25 11:05:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -9,6 +9,7 @@ from sklearn.preprocessing import StandardScaler
 import pickle
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import classification_report
+from xgboost import XGBClassifier
 
 
 
